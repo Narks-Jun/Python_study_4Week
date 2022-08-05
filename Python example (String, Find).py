b1af9c9723c5bc5179b5795e6f42547d23b136c7
Narks_Jun <110293123+Narks-Jun@users.noreply.github.com>
2022-08-05 19:55:17 +0900

    Delete Python example (String, Find).py

--- Python example (String, Find).py
+++
@@ -1,9 +0,0 @@
-str = 'X-DSPAM-Confidence: 0.8475'
-
-ipos = str.find(':')
-# print(ipos)
-piece = str[ipos+2:]
-# print(piece)
-# print(piece+42.0) This will failed because piece is string
-value = float(piece)
-print(value)
